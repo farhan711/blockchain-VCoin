225a0fa07974caf11090920ee7ab9273b721c656
Farhan Ansari <farhan.ansari@rapidcircle.com>
2018-02-13 11:52:58 +0530

    database addon

---
+++ database.py
@@ -0,0 +1,56 @@
+from multiprocessing import Process
+import os
+import json
+
+def default_entry(): return dict(count=0, amount=0)
+
+def _noop():
+    return None
+
+class DatabaseProcess(Process):
+    '''
+    Manages operations on the database.
+    '''
+    def __init__(self, heart_queue, database_name, logf, port):
+        super(DatabaseProcess, self).__init__(name='database')
+        self.heart_queue = heart_queue
+        self.database_name = database_name
+        self.logf = logf
+        self.port = port
+
+    def get(self, args):
+        '''Gets the key in args[0] using the salt'''
+        try:
+            return json.loads(self._get(self.salt + str(args[0])))
+        except KeyError:
+            return default_entry()
+
+    def put(self, args):
+        '''
+        Puts the val in args[1] under the key in args[0] with the salt
+        prepended to the key.
+        '''
+        self._put(self.salt + str(args[0]), json.dumps(args[1]))
+
+    def existence(self, args):
+        '''
+        Checks if the key in args[0] with the salt prepended is
+        in the database.
+        '''
+        try:
+            self._get(self.salt + str(args[0]))
+        except KeyError:
+            return False
+        else:
+            return True
+
+    def delete(self, args):
+        '''
+        Removes the entry in the database under the the key in args[0]
+        with the salt prepended.
+        '''
+        # It isn't an error to try to delete something that isn't there.
+        try:
+            self._del(self.salt + str(args[0]))
+        except:
+            pass
