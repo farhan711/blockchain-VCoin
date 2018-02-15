0684d78726e07b72ff8f1b3f40995d6c8f52ff15
Farhan Ansari <farhan7015@gmail.com>
2018-02-15 15:43:07 +0530

    timers added to the target

--- target.py
+++ target.py
@@ -31,4 +31,34 @@ def target(length=0):
         out=memoized_weights[:length]
         out.reverse()
         return out
-  
+    def estimate_target():
+        """
+        We are actually interested in the average number of hashes required to
+        mine a block. number of hashes required is inversely proportional
+        to target. So we average over inverse-targets, and inverse the final
+        answer. """
+        def sumTargets(l):
+            if len(l) < 1:
+                return 0
+            while len(l) > 1:
+                l = [blockchain.hexSum(l[0], l[1])] + l[2:]
+            return l[0]
+        targets = blockchain.recent_blockthings('targets', custom.history_length)
+        w = weights(len(targets))#should be rat instead of float
+        tw = sum(w)
+        targets = map(blockchain.hexInvert, targets)
+        def weighted_multiply(i):
+            return targetTimesFloat(targets[i], w[i]/tw)#this should use rat division instead
+        weighted_targets = [weighted_multiply(i) for i in range(len(targets))]
+        return blockchain.hexInvert(sumTargets(weighted_targets))
+    def estimate_time():
+        times = blockchain.recent_blockthings('times', custom.history_length)#should be deterministally turned into rats
+        times=map(Decimal, times)
+        blocklengths = [times[i] - times[i - 1] for i in range(1, len(times))]
+        w = weights(len(blocklengths))  # Geometric weighting
+        tw = sum(w)
+        return sum([w[i] * blocklengths[i] / tw for i in range(len(blocklengths))])
+    retarget = estimate_time() / custom.blocktime
+    return targetTimesFloat(estimate_target(), retarget)
+
+
